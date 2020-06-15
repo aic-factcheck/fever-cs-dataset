4dde6a7d13b13fe6c46d969b1c32371717e25c42
Herbert Ullrich <ja@bertik.net>
2020-06-15 09:59:41 +0200

    scripts to init DrQA

---
+++ build_db.sh
@@ -0,0 +1,4 @@
+/home/bertik/miniconda3/bin/python3.7 \
+/home/bertik/miniconda3/lib/python3.7/site-packages/drqascripts/retriever/build_db.py \
+/home/bertik/diplomka/fever-cs-dataset/fever_cs.jsonl \
+/home/bertik/diplomka/fever-cs-dataset/fever_cs_.db
---
+++ build_tfidf.sh
@@ -0,0 +1,7 @@
+/home/bertik/miniconda3/bin/python3.7 \
+/home/bertik/miniconda3/lib/python3.7/site-packages/drqascripts/retriever/build_tfidf.py \
+--ngram 2 \
+--hash-size 16777216 \
+--tokenizer simple \
+/home/bertik/diplomka/fever-cs-dataset/fever_cs_.db \
+/home/bertik/diplomka/fever-cs-dataset/tfidf/
