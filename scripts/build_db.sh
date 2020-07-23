f0b5e6b9bfb9648248c19c1cb17f88790d33c2ab
Herbert Ullrich <ullriher@fel.cvut.cz>
2020-07-23 18:51:55 +0200

    Towards standard data structure

--- scripts/build_db.sh
+++ scripts/build_db.sh
@@ -1,1 +1,1 @@
-PYTHONPATH=../src python ../src/scripts/build_db.py ${1:-../data/wiki-pages} ${2:-../data/fever.db}
+PYTHONPATH=../src python ../src/scripts/build_db.py ${1:-../data/wiki-pages} ${2:-../data/index/fever.db}
